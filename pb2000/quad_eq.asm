4680595c08523a9c0c0a28e06cde087fe99822b0
Wojciech Owczarek <wojciech@owczarek.co.uk>
2021-07-28 15:29:06 +0100

    Comment updates

--- pb2000/quad_eq.asm
+++ pb2000/quad_eq.asm
@@ -8,16 +8,16 @@ START here
 ORG BASE
 here:
     ; check if two 4-byte sequences are equal. faster and not broken like DL-Pascal's longint comparison
-    ldm $8, (IX+$31),4   ; load addr ipa and ipb into $8..$11
-    pre IZ,$8            ; iz = ipa
-    ldm $0, (IZ+$31),4   ; $0...$3 = ipa
-    pre IZ,$10           ; iz = ipb
-    ldm $4, (IZ+$31),4;  ; $4...$7 = ipb
+    ldm $8, (IX+$31),4   ; load addr a and b into $8..$11
+    pre IZ,$8            ; iz = a
+    ldm $0, (IZ+$31),4   ; $0...$3 = a
+    pre IZ,$10           ; iz = b
+    ldm $4, (IZ+$31),4;  ; $4...$7 = b
     ld $10,3             ; constant, IX offset of a boolean return value is -3
-    xrcm $0,$4,4         ; xor ipa with ipb
+    xrcm $0,$4,4         ; xor a with b
     jr nz,neq            ; not equal
     ld $0,$30,jr res     ; equal, $0=1, set result
 neq:
     ld $0,$31            ; not equal, $0=0
 res:
-    st $0,(IX-$10)
+    st $0,(IX-$10)       ; store return value
