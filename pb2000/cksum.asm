02f693170fd66a681f4998ecb43b3dc52fe862cc
Wojciech Owczarek <wojciech@owczarek.co.uk>
2021-07-30 15:17:17 +0100

    Comments

--- pb2000/cksum.asm
+++ pb2000/cksum.asm
@@ -12,6 +12,9 @@ here:
     ; but behold, the wonder of mathematics! the sum is byte-order independent!
 
     ; 21 instructions, 51 bytes of machine code.
+    ; can be reduced to 20 instructions if we move ldiw before jr in loop:
+    ; and then skip the ld in lastbyte:, but this means we always read
+    ; 1 or 2 bytes past the buffer. this is safe, but, principles...
 
     ; $SZ=>$0,$SY=>$30(=1),$SX=>$31(=0)
 
@@ -23,6 +26,8 @@ here:
     xrw $6, $6      ; zeroize $6 = sum
     ; we will now be using $0..$1 to load the words, 
     ; because it is available as specific register $SZ, saving 3 cycles on adw
+    ; hd61700 assembler optimises $0/30/31 to $Z/Y/X automatically,
+    ; but we use them here explicitly
 loop:
     ; you would expect sbcw first (check), jump on carry, then sbw (subtract)...
     ; but we decrement in advance, and thus save an extra instruction per loop. WIN.
@@ -33,7 +38,8 @@ loop:
     jr nc, loop     ; no carry = continue
     adw $6,$SY,loop ; extra 1 for carry and continue
 lastbyte:
-    adw $4, $2      ; we end up with -1 or -2 here, so add it back
+    ; this also works as a check, which we would have done anyway
+    adw $4, $2      ; we only end up with -1 or -2 here. add back the 2
     jr z, end       ; 0=done, the only other possible $4 value is 1 = last byte
     ld $0, (IZ+$SX) ; last byte into $0
     byu $1          ; zero into $1
@@ -42,5 +48,5 @@ lastbyte:
     adw $6,$SY      ; extra 1 for carry
 end:
     invw $6         ; invert
-    biu $2          ; turn the 2 into 4
+    biu $2          ; turn the 2 into 4 because we can (was: ld $4,4)
     stw $6, (IX-$2) ; result! (IX-4)
