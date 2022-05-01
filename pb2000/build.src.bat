6d048abe5e79467713cfdb2201fb5ee24325779f
Wojciech Owczarek <wojciech@owczarek.co.uk>
2022-05-01 22:10:55 +0100

    Clear screan during build stages

--- pb2000/build.src.bat
+++ pb2000/build.src.bat
@@ -1,12 +1,16 @@
 { Copyright (c) 2021, Wojciech Owczarek }
 { All rights reserved }
 { BSD 2-clause licence - see LICENSE in the top directory }
-
-@echo Builiding PBNET library...
+@cls
+@echo Builiding PBNET unit...
 @comp PBNET.INT
+@cls
+@echo Builiding PBNET library...
 @comp PBNET.PAS
+@cls
 @echo Building net.exe tool...
 @comp net.pas
+@cls
 @echo Cleaning up...
 @del net.pas
 @del PBNET.INT
