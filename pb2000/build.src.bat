8d16c98b494c90ad939ec21e6517d6f97e61a940
Wojciech Owczarek <wojciech@owczarek.co.uk>
2023-06-22 17:17:34 +0100

    Added 'noclean' parameter to keep sources after build

--- pb2000/build.src.bat
+++ pb2000/build.src.bat
@@ -1,6 +1,7 @@
 { Copyright (c) 2021, Wojciech Owczarek }
 { All rights reserved }
 { BSD 2-clause licence - see LICENSE in the top directory }
+@XXnoclean=1
 @cls
 @echo Builiding PBNET unit...
 @comp PBNET.INT
@@ -11,9 +12,14 @@
 @echo Building net.exe tool...
 @comp net.pas
 @cls
+@if XX%1=1 then goto noclean
 @echo Cleaning up...
 @del net.pas
 @del PBNET.INT
 @del PBNET.PAS
 @echo Done!
 @del build.bat
+@goto end
+:noclean
+@echo Done. Source files not removed.
+:end
